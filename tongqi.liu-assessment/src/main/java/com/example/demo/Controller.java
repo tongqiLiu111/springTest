fda087987482aa691768c3f8731481fe6149a728
tongqiLiu111 <tongqi.liu@ibm.com>
2023-11-21 17:14:09 -0400

    assessment result v1

---
+++ tongqi.liu-assessment/src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class Controller {
+    @Value("${MY_MESSAGE:aDefaultMessage}")
+    private String Message;
+
+    @GetMapping("/reverse")
+    public String test(String string){
+        return new StringBuilder(string).reverse().toString();
+    }
+
+    @GetMapping("/display")
+    public String getENV() {
+
+        return Message;
+    }
+
+}
---
+++ tongqi.liu-assessment/src/test/java/com/example/demo/SpringbootsTestApplicationTests.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(Controller.class)
+class SpringbootsTestApplicationTests {
+
+	@Autowired
+	protected MockMvc mvc;
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+
+
+	@Before
+	public void setUp() {
+		mvc = MockMvcBuilders.standaloneSetup(new Controller()).build();
+	}
+
+	@Test
+	void getreverse() throws Exception{
+		mvc.perform(MockMvcRequestBuilders
+						.get("/reverse")
+				.accept(MediaType.APPLICATION_JSON_UTF8_VALUE)
+				.param("string","word"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string("drow"))
+				.andDo(MockMvcResultHandlers.print());
+
+	}
+
+	@Test
+	void getENV() throws Exception{
+
+		mvc.perform(MockMvcRequestBuilders
+						.get("/display")
+						.accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string("aDefaultMessage"))
+				.andDo(MockMvcResultHandlers.print());
+	}
+
+
+
+}
